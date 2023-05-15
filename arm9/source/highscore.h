39175911db97935a735fcb04adbf6199448e28ce
wavemotion-dave <75039837+wavemotion-dave@users.noreply.github.com>
2023-05-15 12:46:03 -0400

    Added new High Score files

---
+++ arm9/source/highscore.h
@@ -0,0 +1,12 @@
+#ifndef __HIGHSCORE_H
+#define __HIGHSCORE_H
+
+#include <nds.h>
+
+#include "cartridge.h"
+
+extern void highscore_init(void);
+extern void highscore_save(void);
+extern void highscore_display(void);
+
+#endif
