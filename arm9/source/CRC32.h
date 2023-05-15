e4741f720365b043338c772e69e5e780a6d79231
wavemotion-dave <75039837+wavemotion-dave@users.noreply.github.com>
2023-05-15 06:37:34 -0400

    Added CRC32 utilities

---
+++ arm9/source/CRC32.h
@@ -0,0 +1,27 @@
+/*
+ * CRC32.c contains the file CRC32 utility
+ * 
+ * A8DS - Atari 8-bit Emulator designed to run on the Nintendo DS/DSi is
+ * Copyright (c) 2021-2023 Dave Bernazzani (wavemotion-dave)
+
+ * Copying and distribution of this emulator, its source code and associated 
+ * readme files, with or without modification, are permitted in any medium without 
+ * royalty provided this full copyright notice (including the Atari800 one below) 
+ * is used and wavemotion-dave, alekmaul (original port), Atari800 team (for the
+ * original source) and Avery Lee (Altirra OS) are credited and thanked profusely.
+ * 
+ * The A8DS emulator is offered as-is, without any warranty.
+ * 
+ * Since much of the original codebase came from the Atari800 project, and since
+ * that project is released under the GPL V2, this program and source must also
+ * be distributed using that same licensing model. See COPYING for the full license.
+ */
+
+#ifndef CRC32_H
+#define CRC32_H
+#include <nds.h>
+
+u32 getFileCrc(const char* filename);
+
+#endif
+
